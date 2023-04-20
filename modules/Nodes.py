49cf7201c538104ccbd66b379a829d5eb630ec68
Cliff-Ezra <cliffezra212@gmail.com>
2023-04-20 13:38:27 +0300

    Modified identifier detection on coee

--- modules/Nodes.py
+++ modules/Nodes.py
@@ -30,8 +30,10 @@ class VarAssignNode:
     def __init__(self, var_name_tok, value_node):
         self.var_name_tok = var_name_tok
         self.value_node = value_node
-        self.pos_start = self.var_name_tok.pos_start
-        self.pos_end = self.value_node.pos_end
+
+    def __str__(self):
+        return f"({self.var_name_tok}, {self.value_node})"
+
 
 
 class BinOpNode:
